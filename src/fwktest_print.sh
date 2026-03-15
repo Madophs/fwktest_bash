26c50fc6b6d45bba586ea7ad9dcba4f9477c228c
madophs <jehuruvj@gmail.com>
2026-03-15 08:25:28 -0600

    Color seconds

--- src/fwktest_print.sh
+++ src/fwktest_print.sh
@@ -2,6 +2,7 @@
 
 __RED='\e[1;31m'
 __GREEN='\e[1;32m'
+__GREEN2='\e[0;32m'
 __YELLOW='\e[1;33m'
 __BLUE='\e[1;34m'
 __PURPLE='\e[1;35m'
@@ -14,7 +15,7 @@ function fwktest_print() {
     shift
     case ${color} in
         FAIL)
-            printf "${__BLUE}[${__RED} FAIL ${__BLUE}]${__BLK} ${__PURPLE}%b${__BLK}::${__CYAN}%d${__BLK} => %b\n" "${FUNCNAME[2]}" "${BASH_LINENO[1]}"  "${1}" >&2
+            printf "${__BLUE}[${__RED} FAIL ${__BLUE}] ${__RED}%b${__BLK}::${__CYAN}%d${__BLK} => %b\n" "${FUNCNAME[2]}" "${BASH_LINENO[1]}"  "${1}" >&2
             return 1
         ;;
         ERROR)
@@ -22,19 +23,19 @@ function fwktest_print() {
             exit 1
             ;;
         FAILED)
-            printf "${__BLUE}[${__RED}FAILED${__BLUE}]${__BLK} ${__PURPLE}%b${__BLK} ${__BLUE}(${__YELLOW}%b${__BLUE})${__BLK}\n" "${1}" "${2}" >&2
+            printf "${__BLUE}[${__RED}FAILED${__BLUE}] ${__PURPLE}%b ${__BLUE}(${__YELLOW}%b${__BLUE}) ${__CYAN}Time: ${__GREEN2}%s${__BLK} seconds.\n" "${1}" "${2}" "${3}">&2
         ;;
         PASS|PASSED)
-            printf "${__BLUE}[${__GREEN}PASSED${__BLUE}]${__BLK} ${__PURPLE}%b${__BLK} ${__BLUE}(${__YELLOW}%b${__BLUE})${__BLK}\n" "${1}" "${2}" >&2
+            printf "${__BLUE}[${__GREEN}PASSED${__BLUE}] ${__PURPLE}%b ${__BLUE}(${__YELLOW}%b${__BLUE}) ${__CYAN}Time: ${__GREEN2}%s${__BLK} seconds.\n" "${1}" "${2}" "${3}" >&2
         ;;
         TESTS_FAILED)
-            printf "${__BLUE}[${__RED}TESTS NOT PASSED${__BLUE}]${__BLK} ${__GREEN}PASSED:${__BLK} %b ${__RED}FAILED:${__BLK} %b ${__YELLOW}TOTAL:${__BLK} %b ${__CYAN}Time: ${__BLK}%s seconds\n" "${1}" "${2}" "${3}" "${4}" >&2
+            printf "${__BLUE}[${__RED}TESTS NOT PASSED${__BLUE}]${__GREEN} PASSED: %b ${__RED}FAILED: %b ${__YELLOW}TOTAL: %b ${__CYAN}Time: ${__GREEN2}%s${__BLK} seconds\n" "${1}" "${2}" "${3}" "${4}" >&2
         ;;
         TESTS_PASSED)
-            printf "${__BLUE}[${__GREEN}ALL TESTS PASSED${__BLUE}]${__BLK} ${__GREEN}PASSED:${__BLK} %b ${__CYAN}Time: ${__BLK}%s seconds\n" "${1}" "${2}" >&2
+            printf "${__BLUE}[${__GREEN}ALL TESTS PASSED${__BLUE}]${__GREEN} PASSED: %b ${__CYAN}Time: ${__GREEN2}%s${__BLK} seconds\n" "${1}" "${2}" >&2
         ;;
         STATUS)
-            printf "${__BLUE}[${__CYAN}STATUS${__BLUE}]${__BLK} %b ${__BLUE}(${__YELLOW}%b${__BLUE})${__BLK}\n\n" "${1}" "${2}" >&2
+            printf "${__BLUE}[${__CYAN}STATUS${__BLUE}]${__BLK} %b ${__BLUE}(${__YELLOW}%b${__BLUE}) ${__CYAN}Time: ${__GREEN2}%s${__BLK} seconds\n\n" "${1}" "${2}" "${3}" >&2
         ;;
     esac
 }
