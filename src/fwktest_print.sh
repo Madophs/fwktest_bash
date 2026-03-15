f269eb42a353096451fe12905976eff04312ea78
madophs <jehuruvj@gmail.com>
2026-03-15 04:56:08 -0600

    Add error message and quote func args

--- src/fwktest_print.sh
+++ src/fwktest_print.sh
@@ -17,6 +17,10 @@ function fwktest_print() {
             printf "${__BLUE}[${__RED} FAIL ${__BLUE}]${__BLK} ${__PURPLE}%b${__BLK}::${__CYAN}%d${__BLK} => %b\n" "${FUNCNAME[2]}" "${BASH_LINENO[1]}"  "${1}" >&2
             return 1
         ;;
+        ERROR)
+            printf "${__BLUE}[${__RED}ERROR${__BLUE}]${__BLK} %s\n" "${*}" >&2
+            exit 1
+            ;;
         FAILED)
             printf "${__BLUE}[${__RED}FAILED${__BLUE}]${__BLK} ${__PURPLE}%b${__BLK} ${__BLUE}(${__YELLOW}%b${__BLUE})${__BLK}\n" "${1}" "${2}" >&2
         ;;
@@ -24,10 +28,10 @@ function fwktest_print() {
             printf "${__BLUE}[${__GREEN}PASSED${__BLUE}]${__BLK} ${__PURPLE}%b${__BLK} ${__BLUE}(${__YELLOW}%b${__BLUE})${__BLK}\n" "${1}" "${2}" >&2
         ;;
         TESTS_FAILED)
-            printf "${__BLUE}[${__RED}TESTS NOT PASSED${__BLUE}]${__BLK} ${__GREEN}PASSED:${__BLK} %b ${__RED}FAILED:${__BLK} %b ${__YELLOW}TOTAL:${__BLK} %b ${__CYAN}Time: ${__BLK}%.3f seconds\n" "${1}" "${2}" "${3}" ${4} >&2
+            printf "${__BLUE}[${__RED}TESTS NOT PASSED${__BLUE}]${__BLK} ${__GREEN}PASSED:${__BLK} %b ${__RED}FAILED:${__BLK} %b ${__YELLOW}TOTAL:${__BLK} %b ${__CYAN}Time: ${__BLK}%s seconds\n" "${1}" "${2}" "${3}" "${4}" >&2
         ;;
         TESTS_PASSED)
-            printf "${__BLUE}[${__GREEN}ALL TESTS PASSED${__BLUE}]${__BLK} ${__GREEN}PASSED:${__BLK} %b ${__CYAN}Time: ${__BLK}%.3f seconds\n" "${1}" ${2} >&2
+            printf "${__BLUE}[${__GREEN}ALL TESTS PASSED${__BLUE}]${__BLK} ${__GREEN}PASSED:${__BLK} %b ${__CYAN}Time: ${__BLK}%s seconds\n" "${1}" "${2}" >&2
         ;;
         STATUS)
             printf "${__BLUE}[${__CYAN}STATUS${__BLUE}]${__BLK} %b ${__BLUE}(${__YELLOW}%b${__BLUE})${__BLK}\n\n" "${1}" "${2}" >&2
